7cc964800d1e06cf372a2b96350d9ae81cb8e511
jmpleo <totrash@duck.com>
2024-09-18 22:14:50 +0300

    dev up

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,7 @@ RUN apt-get update && apt-get install -y \
     wget https://github.com/eosio/eosio.cdt/releases/download/v1.8.0/eosio.cdt_1.8.0-1-ubuntu-18.04_amd64.deb && \
     apt-get install -y ./eosio.cdt_1.8.0-1-ubuntu-18.04_amd64.deb && \
     cleos wallet create -f /tmp/cleos_default_pass && \
-    cleos wallet create_key | egrep 'EOS[a-zA-Z0-9]{50}' --only-match > /tmp/cleos_pub_key && \
+    cleos wallet create_key | egrep 'EOS[a-zA-Z0-9]{50}' --only-match > /tmp/cleos_default_pub_key && \
     cleos wallet import --private-key '5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3' && \
     chmod +x entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-keosd > /dev/null 2>&1 & nodeos -e -p eosio \
---plugin eosio::producer_plugin \
---plugin eosio::producer_api_plugin \
---plugin eosio::chain_api_plugin \
---plugin eosio::http_plugin \
---plugin eosio::history_plugin \
---plugin eosio::history_api_plugin \
---filter-on="*" \
---access-control-allow-origin='*' \
---contracts-console \
---http-validate-host=false \
---verbose-http-errors >> nodeos.log 2>&1 &
+keosd > /dev/null 2>&1 &
+nodeos -e -p eosio \
+    --plugin eosio::producer_plugin \
+    --plugin eosio::producer_api_plugin \
+    --plugin eosio::chain_api_plugin \
+    --plugin eosio::http_plugin \
+    --plugin eosio::history_plugin \
+    --plugin eosio::history_api_plugin \
+    --filter-on="*" \
+    --access-control-allow-origin='*' \
+    --contracts-console \
+    --http-validate-host=false \
+    --verbose-http-errors >> nodeos.log 2>&1 &
+
+# create account for developer associated with pubkey of wallet default
+cleos create account eosio dev $(cat /tmp/cleos_default_pub_key)
+
+tail -f nodeos.log
+
+
